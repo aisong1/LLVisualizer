0a6118663cee48d68f2b2b5ea4a15cf373a74c2e
Andrew Song <andrewsong61@gmail.com>
2021-03-08 10:54:08 -0800

    Add Header component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Header from './Header';
 
 const App = () => (
-  <div>
-    If you can see this, React is working!
-  </div>
+  <Header />
 );
 
 export default App;
