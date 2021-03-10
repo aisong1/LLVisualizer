b765ff399b57881f47c71bdef818ed6d8654579f
Andrew Song <andrewsong61@gmail.com>
2021-03-10 15:31:10 -0800

    Apply global styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Header from './Header';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #131313;
+    color: #ffffff;
+  }
+`;
 
 const App = () => (
-  <Header />
+  <div>
+    <GlobalStyle />
+    <Header />
+  </div>
 );
 
 export default App;
