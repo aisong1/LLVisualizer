28433e98c35b3f31aa62a08a64c167b900e6b880
Andrew Song <andrewsong61@gmail.com>
2021-03-10 10:30:10 -0800

    Create barebone header component

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #1f1f1f;
+  background-size: 100%;
+  margin-right: 20%;
+  cursor: pointer;
+  border: 1px solid #1f1f1f;
+
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+`;
+
+const Github = styled.img`
+  width: 100%;
+`;
+
+const Heading = styled.h1`
+  font-size: 32pt;
+  padding-left: 20%;
+`;
+
+const Wrapper = styled.div`
+  background-color: #1f1f1f;
+  color: #ffffff;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const openInNewTab = (url) => {
+  const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+  if (newWindow) {
+    newWindow.opener = null;
+  }
+}
 
 const Header = () => {
   return (
-    <header className="Header">
-      <h1>LL Visualizer</h1>
-    </header>
+    <Wrapper className="Header">
+      <Heading>LL Visualizer</Heading>
+      <Button aria-label="GitHub" onClick={() => {
+        openInNewTab('https://www.github.com/aisong1/LLVisualizer');
+      }}>
+        <Github src="../img/GitHub-Mark-Light-64px.png" />
+      </Button>
+    </Wrapper>
   )
 };
 
